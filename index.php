3155814dfc83c014b6934f41b6c4d0431cb7a437
ShinjiSKA <95872475+ShinjiSKA@users.noreply.github.com>
2021-12-10 19:59:29 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="shortcut icon" type="imagex/png" href="https://lh3.googleusercontent.com/TsioznLUYsBAkDDz1mMkId8A2r_vJ0feBXC5FQpq2sFf_M6HUPz8xFLaUB91gg2Lz7URmlwpLx5cSu0we_0o2WuEG87RfXFe4Oj0iYjpkMZ2tM9GmoOYpyuP23vzDsnR7MlsBkManDPThr0upKzb5jRwGwN23gwBe0duxkO2oafgjAVqXOWJY3xYWfaYjgvc5vTrgM2bw2JAyV4GfG4XMcBEQ3CmCAxQETySb5SPBre6mOadT_T1VdeT9lIms24Oc-7rJenfNChRxRvZUX23b_7A-DYO4FCzdHZbz_8JuCqlOMIiUneE8IH3Q0_zBZVzWDq1ZVl2ujf5ely8YW2ouk3n-1zUJ7cOzxlcK4InvP_Xd3qjcie3sehJVrYtjeo8VXoU2EOO6qoLQpvSUCsjPy_1NozEb0rT07nDmumd-HYq29GdZ6s_dzBY2grb_jp7kh8Q0zWOeCv6ivZS9cH2Ntz9LDDdY-C5hP7PHAaVwQ2fOmdstgS5DlDOzjceNPtHnjnSz295qEbLsaNCAW1Yc-QxwwJVD5xvFrPUFYRtovf8VTRecYcojJAJs__CxfbcPqgdBpoGMWJKuwCrqwsZP_z0pJbChS-YjWg6PvQ9Hu3FPeea9lZGHikoyWTBBCPihX2E38m7ImwIUHTRkjUoMmA1HNdTgFItklJPfZu38AQ_A56UDrcfBdhQKllA6EOl37aIPbI1MeI33WxA3rWG5LEP=s970-no?authuser=0">
+<title>Bingo The Cat</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+.w3-third img{margin-bottom: -6px; opacity: 0.8; cursor: pointer}
+.w3-third img:hover{opacity: 1}
+</style>
+<body class="w3-light-grey w3-content" style="max-width:1600px">
+
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-bar-block w3-white w3-animate-left w3-text-grey w3-collapse w3-top w3-center" style="z-index:3;width:300px;font-weight:bold" id="mySidebar"><br>
+  <h3 class="w3-padding-64 w3-center"><b>BINGO</b></h3>
+  <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-hide-large">CLOSE</a>
+  <a href="#" onclick="w3_close()" class="w3-bar-item w3-button">PORTFOLIO</a> 
+  <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button">ABOUT ME</a> 
+  <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button">CONTACT</a>
+</nav>
+
+<!-- Top menu on small screens -->
+<header class="w3-container w3-top w3-hide-large w3-white w3-xlarge w3-padding-16">
+  <span class="w3-left w3-padding">BINGO</span>
+  <a href="javascript:void(0)" class="w3-right w3-button w3-white" onclick="w3_open()">☰</a>
+</header>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:300px">
+
+  <!-- Push down content on small screens --> 
+  <div class="w3-hide-large" style="margin-top:83px"></div>
+  
+  <!-- Photo grid -->
+  <div class="w3-row">
+    <div class="w3-third">
+      <img src="https://lh3.googleusercontent.com/jMXK7c7o9GuF0OGl6qiPl4Y8cXv3qhcvOj9rykKyhTFpPJLA9IaBgmD3dZB7hwfazeeSENLAzwWfA5LCBDuRw71hrbpmSUwQYKy09ql04JowpSsWF4iXIUgBFSLavnSfH07YVoq-M3OMvzn6F0KkIFcJSrWCY1Bf1Q58QP9i7OBU80-lBqthauoLot4VsCkmP1eqsCLQlDMwvZSVtf5VHfz9Hu1jwrP9UudqDXNDA7xCtHeAvVHCWtHi3psMg0bv-ql-zcTgLEYhAU8tU5gL54PWyMrqC7HpJ8D-XGjddSkWyk31DRSo7G3N7r0pOB6a68jTmHEcRI6bs6L2rR_dnldUtyxE1fVeW1TJ8jXmf72IW2QvbXWj30dZEk4aGCgGEkg56suYHRW1J5hEIJxMRPD5W3UO2ESDaso9xGkKVLWSQG3baB8lxuIG-Eonfp92JsI9GUhruOTRAC6pJ9aS5GiFTKCdZt2vHiLnW9H3q8K61R7C4sjSMVVhdzUfiBwtbOGiTZVjQ_Fhbx1V8U5yVjwuq0zkIC1RlRg-lngxJZ8Y-O2-wsV5YcZkjgSU4cBxkHvtZTQMJ-Z3ncpDtyoFWfPipHeMsERFNSTSAOxMgBqTg9KACTNhqgHp_r-FazRsfla4QQ6sLWXh4IAieyEgIANpXlQWFU4FaQg68anOMaHkgMsTA3_QseQvZ4yDYK6SmXkL1eFVtxY26p2MgQcxKGpf=w728-h970-no?authuser=0" style="width:100%" onclick="onClick(this)" alt="A boy surrounded by beautiful nature">
+      <img src="https://lh3.googleusercontent.com/SB_Ib3tYsmfeuO2ZuTF2pLshRA809dMHet6Ce5V_vDXFiiHugbg5LmDH7tTzP-hr_LYczRwqDiqiDQ8mCdPgn8OInrKM-UzwhKHGz0_n71TRcAohG2xd2q1kyRO-KEvyJadyqSfkAkhqGEQSxuMEPGZDEoS5b_668c9Rv2ipJTd-JxbrAHYAvwqzp1biO905snjCAi2TWVp5JGfBBGsFGWaJa3TTJ6j8sPxtAVUcs8nDh7TJS_YEW1SphMkSshNazazZ1dJfRNEy8hFiMe8AvFaTK2ng-LWu09NPQj-hqf82N_2VdHSdiDV_-90mgy6_Sz3vd4XPa8zPPU2W_e5j97SZmH66Sn8CtnODR9xZ1GOEiYOvFViCtLrPKlXMxzBFUI9SNxMlxYPkkwWiKJo41WyY9-EMZ0-EXvL0MS1bAUQ2AXZynyy0ErVd7A-FI4RnHMm1q8dZ1K_Ckcj2nK8b7KnP7bl6ClkpsjswfYnEuxJQ7F-hGaoMTDXXns-94V_gpndOqEbWd41ZebHrgjfG1S6wOm9boTHkaZSQyUzylv96WcicZBp4UAVIQPkHQjlX2bsmniw125aSlRQ4U04uD4gVRTcx8WkKYZVf7D4Sra6_3GvsnxSYvpFWJssSnexSTgLWuCuF3aZ0Cs6kl3s4GscNI7tbbxSIMitJPFcJMgmRXtwg9MTFDid-wvjxGeImpmpd9i-zEVNovsnh4Q4Z4MHM=w728-h970-no?authuser=0" style="width:100%" onclick="onClick(this)" alt="Sleeping">
+      <img src="https://lh3.googleusercontent.com/FqfcFGQRQySBf5CCBAz8UE-fitNcJp-6fkDPGrQ85gTrEaXbRG2-4FErvjz-By8azA1PcSb8Gz-1HVNgPYQOSpGoUBcEvCbRnJ5CIFBVsl4JTU3C5yF2ArvHNxhYJzbh0SexQsyZsvh5JvXT8qjYkqsClBFNkrmhPOYCNyTHJ6zNylBq0_vzPhP6XdUAFt8zuU3rqEFPOGZBAOUB5xeHfVtu6tyV2F2cpugGKlgSJNr0kzkKDXvyF-doxNJFuLZPAI6X8VoXdKqmzxk6SCtU9LiXnjh8IqohnQf99tsxOWCoFMkIy63iJp19UywbBy4jzJwc0Py19bpymxafTdTibD815v6R831uhwMCepdu2Yn27JaQ8DqUzc0e7uuHemlEk80jbOgkNOx-JUYAVEFWxanvZA-twNv4eZnpXqVyWSTtNhn1SV0ghnTLvMyp2DixxzsBqczUVdidqmhWdLltdP9un8mlmHdPBgq15NqNRG1aAgau8dyC5eqDWqrs4s1boCYynVi4WfTqJnNp6L1S5vVrJxVIH9bCWWnSWG8nbASIYJKcj0ReqCLCNqo6_CZDlNpgGlORIPrWTfT8mszXymX3m9uHEe1nkADAFLt36qMJ0dA7Hum5g-HQ-pXpu9-UW1ylQCBGdYgxKm990xU7pVAVTjJ0tVSpodbJK4pUO_wVNz6pkUVqV-EO_cGuXzL9Rh58V6Naq72UogZeOg01eGqc=w728-h970-no?authuser=0" style="width:100%" onclick="onClick(this)" alt="ARE U LOOKING AT ME??">
+    </div>
+
+    <div class="w3-third">
+      <img src="https://lh3.googleusercontent.com/EG0-9HKGX0xbix8bswrNQiWoGraao5USVSSu9dqPUvecyLr3GBUqIPQvF_7OmzeF_De8T_5yerBxyisPN1NmIv66oiBsX0-vbtmnJpXWn3_b69_WQ-bn9BTjnvBnZiDdkk_cU_iquL3G8lr1yFCW57rhDKFuWNhmce4kUd9YO7-oK_XRmLi1hWTNyrc83S3_yKQcq80KuVJdykSuAwmNdbRxD0DOujkC6zGmvhRqivoPZHt8TlyDhbZhA38bukcBFBgwEeEib9HfjksGCFcdfc9PFrxqTX3-k3gDlle9mMK6IFQBvG8bNvxkJm2EtkTBpol0AVrpEiJ3HzTtF5NC6cSl7fmd3rHW_N5vDUvwI29W6jR7OeEGTbaImsy6lvOLWtPv8ssJ1-DU1NCfnp_ZbvebOiqkelO3WDuKDTI0-Xvxs5ZneTDCt8CWE13EGbVTUHV50cz5efUSYKLN1sLOCIGC5UM3UWSUGQP7HoQjekVdl6ZwhDnNZ_9FoYyc1C6iS9DLEWCc9aHVS1DIixUng3Uw4Mplm2GoMNNf8ZqCsUHFWaCQSSLSpXcVgpSNmsmPM8_tnt6jLxuBHcr8ty8KLRxa_Vt51ZcTOmwAnZUC-iCB-DGHEC1Wp7JhjQr3Wom0c9YTUo7prvxxrTggFPQm35nm_NaAXKMXdt98fI7m8X2EAO1gizH7S6TG9QCAuk7FVcfO6MLZTZTxx83DBrzJXeId=w728-h970-no?authuser=0" style="width:100%" onclick="onClick(this)" alt="Me and my PC">
+      <img src="https://lh3.googleusercontent.com/UAxxah3xVG0EBL06-0hiGE8EB2dKYbSE4Qwuxl4fo-B3q6ulNanOEcxcOucseoMrxe7h5MB3FD3JI0ghUuAV629yIzFnqyiPxYSCbncL_m-DClj4D8_EyzLSEd-QGM4ZXQC9lHUJjavSw98XiVVQQDvsgxovn_1GbMDeEX-uenCT2ty_z9xWlPGo0pBFtSvGHLen-v101XxOl8en4GsxXM6uRvr79RZNQGaNCCjGf4O0JRE3UJQ8zczMrB9d86i5lhHZTjt1PCJFSKsf5cIoR1YL3gnYt7LQuPQlbhZltuO71LIOBZMpmvwyg_ShnDn5BHYc0_fXeMaXrUHGG0zhynEtejfrugCd3DhsEFSkDck7L_qKTJlbEwk_7uoFqn64aQcKupaAoHDdwU3H_E4zuEo57Fn3tnKNlmvDW2y1-WD99PxaWSaQmVoaMVLC9WoafuNmn8gJvFRjc8XodkdZKWeE36HscF-u4y79uA0osUwCKTerDjis0CrefoJgCJAJvmZ3ZwZxYnYMw1icDj5pvoM6TeTm9HDipfNxn70RVkOwmwKTaSD7WL21cWhNOaw0OHYWfX2dESh4kiTJGe28y_XwEjrYqFLrhxsbcHaredb68oMyk2kRwtKBIHWqPHjGsWaKqQ8BbcYev6ninNbWTWMD_LmAwNVV0VPGIikRj5Cx2J-UMH9JM_7gPim1N_CHqUUuPdxxv0e8gGel59AhoBYy=w728-h970-no?authuser=0" style="width:100%" onclick="onClick(this)" alt="YUMM">
+      <img src="https://lh3.googleusercontent.com/Ji0Rs_Bxa7MMrdUd0gYrKJ3Z2r7YASnBZrMDEBHd6b77M11qdqf6b5Xa-tW0ZfqYUQB6ULmS83lCsP2EUc3oN5R3l2zKnhJShDjAhf4BXpxM7OXdKS2RNkWr4akOhnFuYMgmqc43WH1MvIPBl3XJM-aHHY9rgfoWRDOBLYMZFi9ANux-lvk5WEBDUtpN2aLNF2RnDobmzTnCIeKUNN4jr5k0lWhNA8kFV00U-YxQFMkOgqcFlR1vpKOzvAz2YWIzoDkC7B8uQL670YORqjpx6hgPLrrJFKbYL-ZC6QqgiBJeAwDDFPur8jeWHPKmRJlsICyaGpSAUWl5jx3WrANHh1TjrfsTeQ9-7Px-5t6e59rENrbEzngPqmc45vR55tLkqOwK6cEQLQbwwyWIUQuyafyBBvDHPz8t4KZGBZ1awz_obRlVGjP_UBhnQZkq-uE3pbkcX0KosDQNhb7KN9kDdw0gX-NI9lNtXageKX3iGaFqKgd2irQviDEtlp2NqlH3qeJ2lFa_cy1-jpF8oKyLmJv3mgzyAFToU-bJGGpURGY4OOitYUzKDMKTtBs-h1u9La4YzQnR5vop-IeM5cxyevtEja4SOliIDD7HzgQ9oFLizIaGbsKPbEHc0qXC1ZbyChvj04ZiU5gy6jaZPlOJlWXSX-UHtZ4zOHwKWkgR-JOXbsi1vP19bgWbBRS5qTI9XZ065Qd7rx8dyYoef3D1eZAa=w728-h970-no?authuser=0" style="width:100%" onclick="onClick(this)" alt="Sleeping again.. At its finest!">
+    </div>
+    
+    <div class="w3-third">
+      <img src="https://lh3.googleusercontent.com/p8tHpiqKHPdFNwiUuJnlsx2lEmajfQyEc3YMXXz5cR1v-vYbQkNsNlOoE1qSoSX9keDy4aMsm_-lb9SkcDMbPyQvJgSWstGbs4DVniXMR5o0SSg7N4ClL0lgkMhcE7KajytSK7r7ZRfJ2zZuck-D67aed0nseLAV_fhPyNOoWuItkGomTYl3jjClT4b4Rpo3rbP5IhzktQtdeJSXHYE-uraaTEU7WZzcE20Lr5A469q1bQse1TmSZwzaAxpxrqsdakC9WeAgQrIe_BRt6SXbfAaxaAFUHqEpmclXsgPFLqomfJcmLuKlCbB-K3kNHtKnuRzaQes6qSHBKmnV6-fK6_gIAP3Sex_NhJJKy-vIyZ2gEBBjXyfIf3eY4scAdAm-1xe-VWLLew0XEgiljf51TKWoTB8Y_g1tdTEtRYhAWFtMFOCYlEBVlgEoDWwPWKCYQl-gub5RI0ntdmaAznkY9jup3x5m8L2qf9DhtxR3pDJEqz2CtNU1KW0YzORJMwqDOAu1-W7EiiifkuO_pqGPQfm5TGxnfHvbKyphNrf-hexNzIIsBAYcGGesDhzyD647Uw3JFyg9GLzhKAh2q2C_dnyxBsHpVDK1Sg6p0anUNsEwTDTqewOjaEH5M9R_NpVbCz9-jfBhlsyWZJ5EPK5U6S9xvtovjaMp5kvzpYX8l5hXM21vUsgQHszq2tEgOKFuIqmECz4E7ph_VAcwy9nO9R-3=w728-h970-no?authuser=0" style="width:100%" onclick="onClick(this)" alt="Sleeping in my PC">
+      <img src="https://lh3.googleusercontent.com/ssbqtgmHnqHLzu3L7-XVxdk3YPKMfLyAPamFdTz-JUW7PR4ZHhMC0Vs46zvu4f7xtYMLUqjYT4kNpcT6uKw8Oez7cw0MtJoJBmWBGtO1OZxiAyMNUImQTLUDZdQvIRkT7gMbUW8_roVgzZOI_ltTBSXBm10aSzt5O02Zdv99img4LOHyKAoa3MGhjIgnZIt5a8yEoLkxrigoLyxiODLs5DbFEs4-slFLHMh7_Z_iUUelNx5jc-bpVsQS__8UiKxHCd0kJLzLphJ9TctSLa-ok1e5kTxSJhmxMzvGP9qvoMFWjmtNBZ2ZBpTdRH53-gNp-RBuIMiN1_qqjVjaI4Pjch4bTJibMLbS35LgO5om7Q3n0Ird9EFOdtPMQZYKQczrFO98JCNZgqxpFn0iN8qljVm4HHoPunpaR0ct0d-fcXGkoQ5lVfPgjlt7kxHVx8Oc73V1RotX8cEryO0oad7OYbmjSrXozZFc0WjSn_l6sVSuWybAfaXWYjbgmRqoNLNps5toRmwoC5UK54eXEz5njAL7zRUWh6ezq2EO3aI5g0TkHu8D9Vglpf8CySabaJY_RCcZ09oLwyV_6fAPBpOPMKNTskMT_Mz1jmK4YcKYh_M0N2-hxbYN8ZyvaC-7jvHNPSiaVZ3l_Lw14dNU7kUL0IJdeqdR2fZHTPw41n_pU6GeDrm12V8JDq7ga3k9nWbASJo8Om0o44bScO5H9igR-F_u=w728-h970-no?authuser=0" style="width:100%" onclick="onClick(this)" alt="Look at me! I shine">
+      <img src="https://lh3.googleusercontent.com/Yx7n_otrXfa2bDpvifmX9Iaun0rk8KcUIh5GPcFZ0yBmXNA96nOrYclgLqjyhiNknn6K57ZthfQUXlcw0X_CLkJhd8tyjvx35Peo318zagCtt8XfU4b_NR2jXCev6AxQHOAnf0DnqdtvlyjVCQ5m0o8l5CDoXelmvlQCrIwmOMjvtL_hBDJ_HDkHaMMPFlGhkYMmoNDjiw6NNJ9bP9yiBB6aEDLsGJCvTEpcodL6DksAbsBUKfXHza8aUdY0Kh40qWTlSlv_spDAMylAh9u5xG3uKDVDSnb3GVhbID1HQ44YncaCbU_NnNqhi7dLUNUDHoIUHsYNHtZ5nkrP9vCeSmtsqC-H_I8fsiQ0pACBESqiJynk_hsLLqEkTVs3BsZYNk9-4Fqb7hvdITD3Ya6jo8uSa5kQTGaOdKTyawzjk79qHE2BFW7R0wesmgswnM6CxdpZOzFM5Le0NtQXlTzq5i8Q8BrbfAz6O0M3_hcsuV52GAyRb5KAUqWKyQ0vqXXU3leDv4q3a86Rd9HIY1Ed3ir_y9D_cAWu-Wogz8kEGhbPbgjwf3snBoUY0N8yPkNS-6gfzII40cAgn0_-YhknB1FGAwgRMPyacPcfbQ_yGAp7UjFU07f_EZgI4488RG1ZxVOUo1WUZSNhOyX3lGJSn1gGgbpT_FXTl80hGXALUrg9gC7dWFmp2YCRqyo87-2eAs6Df2yRfV1f2dhUrvfnYpjE=w728-h970-no?authuser=0" style="width:100%" onclick="onClick(this)" alt="What a beautiful day!">
+    </div>
+  </div>
+	
+	<div>
+	</div>
+
+  
+  <!-- Modal for full size images on click-->
+  <div id="modal01" class="w3-modal w3-black" style="padding-top:0" onclick="this.style.display='none'">
+    <span class="w3-button w3-black w3-xlarge w3-display-topright">×</span>
+    <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
+      <img id="img01" class="w3-image">
+      <p id="caption"></p>
+    </div>
+  </div>
+
+  <!-- About section -->
+  <div class="w3-container w3-dark-grey w3-center w3-text-light-grey w3-padding-32" id="about">
+    <h4><b>About Me</b></h4>
+    <img src="https://lh3.googleusercontent.com/Z8vS_kD23pAk_IBZMZ-6ECRmP1M7Ee7T6n2NTm2_-ZRkS7r1B6w6dQnd1WJaUEXnUu96SQONTIeD732ro9rQobRT54nwBmLSesGvvUyORMGTBvK6RrcsVtiiE-WNKb-VU7A-QE_fbW8YLGdIGxgQf9yGgjjYN4aAJiF8UIHlTIU1DzUTkd0izDpYxM8u8NOmOj5qDvKLdQMvRhGlCqVclruvoYFm63U27-BwzGVqWde2mo0ds39OVZHxXllDDJoRm0Sym1j_Nt3Ot2CNS8XFuWvs6IwHfI52OKzHVZt2SatNBOuMlkHtNNAWPuRmCzQaK0s9OQeFJJuyJirZ3sGr1d62pyu9Gt2wUNxwqkkq9xjFqFihPaOAkt_wX8q5LrwsBhjamxS583QLR6htOuTerEts3R2QwHjn_PlAglNCZLw7lfivhlAr7r065YU0UUEQMqX9PlDU3PHoNUjMUVVrTHNlIP5jP832PKdGu92IQo89a6niypIOazZBB47NFiHKsC-UuWzGZKEVl0J6wki8PGpsnA93FTc0TQjN_rVVx7R1WjMmoyUhoqSenGD3I0PMhqo8-U4g1KqKm7zWM-QfLtDSW3B8zDrkIWBHsyRtD2JYS-kGuxJjoa7uzgYJW8bkdeTG9qzZ1PTNuxpSRSq252tDEAesE7ZYETNRpypBvgv1iFLcAwBFiK9zfHIT9VcRlCYZO5ujSqL1_tp-vwppgFFC=w356-h355-no?authuser=0" alt="Me" class="w3-image w3-padding-32" width="600" height="650">
+    <div class="w3-content w3-justify" style="max-width:600px">
+      <h4>Bingo</h4>
+      <p>Some text about me... I love biting PEOPLE. I am a cute cat, and I WANT YOUR BLOOD... OPS, I want to give LOVE to everybody =D. Sometimes I love cuddles and sometimes I WILL CUT YOUR FINGER xD.
+      </p>
+      <p>mail: bingo@miau.com</p>
+      <p>tel: 5353 35531</p>
+      <hr class="w3-opacity">
+      <h4 class="w3-padding-16">Technical Skills</h4>
+      <p class="w3-wide">Biting</p>
+      <div class="w3-white">
+        <div class="w3-container w3-padding-small w3-center w3-grey" style="width:95%">95%</div>
+      </div>
+      <p class="w3-wide">Jumping</p>
+      <div class="w3-white">
+        <div class="w3-container w3-padding-small w3-center w3-grey" style="width:85%">85%</div>
+      </div>
+      <p class="w3-wide">Scratching</p>
+      <div class="w3-white">
+        <div class="w3-container w3-padding-small w3-center w3-grey" style="width:80%">80%</div>
+      </div>
+      <p><button class="w3-button w3-light-grey w3-padding-large w3-margin-top w3-margin-bottom">Download Resume</button></p>
+      <hr class="w3-opacity">
+
+    </div>
+  </div>
+
+  <!-- Contact section -->
+  <div class="w3-container w3-light-grey w3-padding-32 w3-padding-large" id="contact">
+    <div class="w3-content" style="max-width:600px">
+      <h4 class="w3-center"><b>Contact Me</b></h4>
+      <p>Do you want me to miau you? Fill out the form and fill me in with the details :) I love biting new people!</p>
+      <form action="/action_page.php" target="_blank">
+        <div class="w3-section">
+          <label>Name</label>
+          <input class="w3-input w3-border" type="text" name="Name" required>
+        </div>
+        <div class="w3-section">
+          <label>Email</label>
+          <input class="w3-input w3-border" type="text" name="Email" required>
+        </div>
+        <div class="w3-section">
+          <label>Message</label>
+          <input class="w3-input w3-border" type="text" name="Message" required>
+        </div>
+        <button type="submit" class="w3-button w3-block w3-black w3-margin-bottom">Send Message</button>
+      </form>
+    </div>
+  </div>
+
+  
+  <div class="w3-black w3-center w3-padding-24">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity">w3.css</a></div>
+
+<!-- End page content -->
+</div>
+
+<script>
+// Script to open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+</script>
+
+
+</body>
+</html>
